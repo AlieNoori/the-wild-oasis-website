8ba3121f1ca1e16c1e2dbd73922c5e2937212f06
alieNoori <alli.nouri.83@gmail.com>
2024-07-03 09:59:36 +0330

    added cabins filter and reservations date

---
+++ app/_components/Reservation.js
@@ -0,0 +1,23 @@
+import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service';
+import DateSelector from './DateSelector';
+import ReservationForm from './ReservationForm';
+
+async function Reservation({ cabin }) {
+  const [settings, bookdeDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+
+  return (
+    <div className="grid min-h-[400px] grid-cols-2 border border-primary-800">
+      <DateSelector
+        settings={settings}
+        bookedDates={bookdeDates}
+        cabin={cabin}
+      />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}
+
+export default Reservation;
